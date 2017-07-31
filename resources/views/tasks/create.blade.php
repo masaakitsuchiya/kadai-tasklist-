f548a111d8ed402536dbc7e34093ddefad65e47a
masaakitsuchiya <zenmon.tiger.koumon.wolf@gmail.com>
2017-07-31 22:49:03 +0000

    controller tasklist done

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<h1>メッセージの新規作成ページ</h1>
+<!-- ここにページ毎のコンテンツを書く -->
+
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+
+@endsection
